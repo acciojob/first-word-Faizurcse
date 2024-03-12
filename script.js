cdd73fb7ae82c812ed36f754a9e5f98a2c9ba61f
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-12 15:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	if(s===''){
+		return s
+	}
+
+	const arr = s.trim().split(' ')
+	return arr[0]
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
+
